9d7e37f54c1aa99f0d4a979cb8b9ea8302c3a36d
CandussoR <r.candusso@protonmail.com>
2022-11-14 16:59:47 +0100

    Creating beta Hero component and modifying LandingPage

---
+++ resources/js/Components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>{{hero}}</p>
+    <p>{{hero.media_url}}</p>
+    <div class="bg-[url({{hero.media_url}})]">
+        <h2>{{hero.title}}</h2>
+        <h4>{{hero.subtitle}}</h4>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                hero: {}
+            }
+        },
+        mounted() {
+            axios.get('api/home/hero')
+                .then(response => this.hero = {...response.data})
+        }
+    }
+</script>
