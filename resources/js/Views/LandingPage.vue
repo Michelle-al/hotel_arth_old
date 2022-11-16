189f58da2fc42570304b50640c916db0a6ed1139
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-11-16 16:54:48 +0100

    Add text and video to video section

--- resources/js/Views/LandingPage.vue
+++ resources/js/Views/LandingPage.vue
@@ -2,17 +2,20 @@
     <h1>I'm the landing page</h1>
     <Hero />
     <Promotional_banner />
+    <VideoSection />
 </template>
 
 <script>
 import Hero from '../Components/Hero'
 import Promotional_banner from "../Components/Promotional_banner"
+import VideoSection from "../Components/VideoSection";
 
     export default {
         name: 'landingPage',
         components: {
             Hero,
-            Promotional_banner
+            Promotional_banner,
+            VideoSection,
         }
     }
 </script>
