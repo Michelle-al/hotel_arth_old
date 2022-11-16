e88a5ca9ea23b1f443b81955f88f0a7c0bd37271
michelle <michelle.alabi@gmail.com>
2022-11-16 13:21:59 +0100

    Promotional_banner component created

--- resources/js/Views/LandingPage.vue
+++ resources/js/Views/LandingPage.vue
@@ -1,9 +1,16 @@
 <template>
     <h1>I'm the landing page</h1>
+
+    <Promotional_banner />
 </template>
 
 <script>
+
+    import Promotional_banner from "../Components/Promotional_banner";
+
     export default {
-        name: 'landingPage'
+        name: 'landingPage',
+
+        components: {Promotional_banner}
     }
 </script>
