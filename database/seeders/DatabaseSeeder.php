0a5f990a8dca3e0ff273d5ba87cbd9f388476cec
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-11-17 10:18:15 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            HeroSeeder::class,
+            PresentationVideoSeeder::class,
+            Promotional_bannerSeeder::class,
+        ]);
     }
 }
