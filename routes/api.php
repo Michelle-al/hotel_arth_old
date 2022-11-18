884d204b8d3298324bb0625b106579f55902e2da
CandussoR <r.candusso@protonmail.com>
2022-11-18 11:28:23 +0100

    Adding room_category api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HeroController;
 use App\Http\Controllers\PromotionalBannerController;
 use App\Http\Controllers\PresentationVideoController;
+use App\Http\Controllers\RoomCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,11 +29,13 @@ Route::put('/home/hero', [HeroController::class, 'update']);
 
 # Promotional Banner API routes
 Route::get('/home/promotional_banner', [PromotionalBannerController::class, 'index']);
-Route::put('/home/promotional_banner', [PromotionalBannerController::class, 'update']);
+Route::post('/home/promotional_banner', [PromotionalBannerController::class, 'update']);
 
 # Presentation-video API routes
 Route::get('/home/presentation_video', [PresentationVideoController::class, 'index']);
-Route::put('/home/presentation_video', [PresentationVideoController::class, 'update']);
+Route::post('/home/presentation_video', [PresentationVideoController::class, 'update']);
 
-
+# RoomCategory API routes
+Route::get('/home/room_category', [RoomCategoryController::class, 'index']);
+Route::post('home/room_category/{id]', [RoomCategoryController::class, 'update']);
 
