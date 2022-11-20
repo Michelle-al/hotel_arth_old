f670e63f7e66d185fece8551f2440563f36a632b
Emilie Ickart <emi.ickart@gmail.com>
2022-11-20 13:22:54 +0100

    Add Customers and Reviews API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HeroController;
 use App\Http\Controllers\PromotionalBannerController;
 use App\Http\Controllers\PresentationVideoController;
+use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\RoomCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -39,3 +41,24 @@ Route::post('/home/presentation_video/{id}', [PresentationVideoController::class
 Route::get('/home/room_category', [RoomCategoryController::class, 'index']);
 Route::post('home/room_category/{id}', [RoomCategoryController::class, 'update']);
 
+
+
+# Avantages API routes
+
+
+
+
+
+# Customers API routes
+Route::post('/home/customers', [CustomerController::class, 'create']);
+Route::get('/home/customers', [CustomerController::class, 'index']);
+Route::put('/home/customers/{id}', [CustomerController::class, 'update']);
+//Route::delete('/home/customers/{id}', [CustomerController::class, 'destroy']);
+
+# Reviews API routes
+Route::post('/home/reviews', [ReviewController::class, 'create']);
+Route::get('/home/reviews', [ReviewController::class, 'index']);
+Route::put('/home/reviews/{id}', [ReviewController::class, 'update']);
+//Route::delete('/home/reviews/{id}', [ReviewController::class, 'destroy']);
+
+#Admin API routes
