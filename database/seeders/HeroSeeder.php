f302535262c501a26f4d7978a931be40cfc46e4b
Emilie Ickart <emi.ickart@gmail.com>
2022-11-20 11:19:25 +0100

    Add fake values to enable running DatabaseSeeder.php

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class HeroSeeder extends Seeder
 {
@@ -14,11 +16,11 @@ class HeroSeeder extends Seeder
     public function run()
     {
         DB::table('heroes')->insert([
-            'title' => ,
-            'title_english' => ,
+            'title' => Str::random(10),
+            'title_english' => '',
             'media_url' => '/storage/hero/hotel-room-g512f9f1ee_1920.jpg',
-            'subtitle' => ,
-            'subtitle_english' => ,
+            'subtitle' => Str::random(20),
+            'subtitle_english' => '',
         ]);
     }
 }
