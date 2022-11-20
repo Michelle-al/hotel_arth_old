00eb1cd9e6288c9ad7efbfc981c20674786bdb8a
Emilie Ickart <emi.ickart@gmail.com>
2022-11-20 11:09:38 +0100

    Replace type string by mediumText for description column in presentation_video_table.

--- database/migrations/2022_11_15_150842_create_presentation_videos_table.php
+++ database/migrations/2022_11_15_150842_create_presentation_videos_table.php
@@ -11,15 +11,15 @@ class CreatePresentationVideosTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('presentation_videos', function (Blueprint $table) {
             $table->id()->nullable(false);
             $table->string('video_url')->nullable(false);
             $table->string('title')->nullable(false);
             $table->string('title_english')->nullable(false);
-            $table->string('description')->nullable(false);
-            $table->string('description_english')->nullable(false);
+            $table->mediumText('description')->nullable(false);
+            $table->mediumText('description_english')->nullable(false);
         });
     }
 
@@ -28,7 +28,7 @@ class CreatePresentationVideosTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('presentation_videos');
     }
