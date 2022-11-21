43bba0f3c81d9dff5afde0a6986c66429602c322
CandussoR <r.candusso@protonmail.com>
2022-11-21 09:29:46 +0100

    Minor changes

--- app/Http/Controllers/RoomCategoryController.php
+++ app/Http/Controllers/RoomCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PresentationVideo;
 use App\Models\RoomCategory;
 use Illuminate\Http\Request;
 
@@ -25,9 +26,12 @@ class RoomCategoryController extends Controller
      * @param  \App\Models\RoomCategory  $roomCategory
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, RoomCategory $roomCategory)
+    public function update(Request $request, int $id)
     {
-        //
+        // Retrieves information stored in DB for video corresponding to the id passed as a parameter of the request.
+        $roomCategory = RoomCategory::query()->find($id);
+        // Stores the content of the request body in a variable.
+        $dataToUpdate = $request->all();
     }
 
     /**
