aca1b1631183db5ba8a75587cbd30660ba43597e
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-11-21 16:57:50 +0100

    Create a components to manage languages flags

---
+++ resources/js/Components/Languages.vue
@@ -0,0 +1,45 @@
+<template>
+    <button @click="isFrench = !isFrench">
+        <img v-if="isFrench"
+             :src="french.image.source"
+             class="section__navbar--flag-english m-0"
+             :alt="french.image.alt">
+        <img v-else
+             :src="english.image.source"
+             class="section__navbar--flag-english"
+             :alt="english.image.alt">
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Languages.vue",
+    data() {
+        return {
+            isFrench: true,
+            french:
+                {
+                    name: 'Français',
+                    href: '#',
+                    image: {
+                        source: '/storage/pictures/flag-french.png',
+                        alt: "Drapeau Français"
+                    },
+                },
+            english: {
+                name: 'English',
+                href: '#',
+                image: {
+                    source: '/storage/pictures/flag-english.png',
+                    alt: "Drapeau Anglais"
+                },
+            },
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
