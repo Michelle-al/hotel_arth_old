da1d889ac5d7634eea14d4c9c1fb621e37d53881
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-12-07 16:10:57 +0100

    Add collapse arrows

--- resources/js/Views/LandingPage/Components/AdvantageItem.vue
+++ resources/js/Views/LandingPage/Components/AdvantageItem.vue
@@ -1,14 +1,38 @@
 <template>
-    <div class="collapse px-4 py-2">
+    <div tabindex="0" class="collapse collapse-arrow px-4 py-2 md:hidden">
         <input type="checkbox" class="peer" />
-        <div class="collapse-title  pr-9 bg-arth-light-blue md:bg-white text-black-content peer-checked:arth-light-blue peer-checked:text-black-content">
-            <div class="flex flex-row md:flex-col">
-                <div class="mb-4 w-1/4 md:w-full h-14"><img alt class="m-auto" :src="advantage.icon_url"></div>
-                <div class="text-center w-3/4 md:w-full ">{{ advantage.title }}</div>
+        <div
+            class="collapse-title px-6 lg:pr-9 bg-arth-light-blue md:bg-white text-black-content peer-checked:arth-light-blue peer-checked:text-black-content">
+            <div class="flex flex-row justify-between items-center md:flex-col">
+                <div class="lg:mb-4 md:w-full h-14">
+                    <img class="m-auto"
+                         :src="advantage.icon_url"
+                         alt=""
+                    >
+                </div>
+                <div class="md:w-full ">
+                    <p class="text-center align-middle pr-3 ">{{ advantage.title }}</p>
+                </div>
             </div>
-
         </div>
-        <div class="collapse-content md:max-h-full md:cursor-default bg-white text-black-content peer-checked:bg-white peer-checked:text-black-content">
+        <div class="collapse-content md:cursor-default md:max-h-full bg-white text-black-content peer-checked:bg-white peer-checked:text-black-content">
+            <p class="text-center pt-8">{{ advantage.description }}</p>
+        </div>
+    </div>
+
+    <div class="px-4 py-2 hidden md:block">
+        <div
+            class="px-6 lg:pr-9 text-black-content">
+            <div class="flex flex-row justify-between items-center md:flex-col">
+                <div class="lg:mb-4 md:w-full h-14">
+                    <img class="m-auto" :src="advantage.icon_url" alt="">
+                </div>
+                <div class="md:w-full ">
+                    <p class="text-center font-bold align-middle ">{{ advantage.title }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="md:cursor-default md:max-h-full bg-white text-black-content">
             <p class="text-center pt-8">{{ advantage.description }}</p>
         </div>
     </div>
@@ -25,3 +49,10 @@
         }
     }
 </script>
+
+<style scoped>
+    .arrow-down {
+        height: 30px;
+        width: auto;
+    }
+</style>
