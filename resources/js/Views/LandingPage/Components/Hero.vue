574c87de002f8ce602b2e02777b8789a3816b8ab
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-12-07 14:06:40 +0100

    Bf- 404 error

--- resources/js/Views/LandingPage/Components/Hero.vue
+++ resources/js/Views/LandingPage/Components/Hero.vue
@@ -1,11 +1,10 @@
 <template>
-    <div :style="{backgroundImage: `url(${hero.media_url})`}" class="flex flex-col bg-cover bg-center h-screen p-24 justify-evenly items-center lg:mb-20">
+    <div :style="image" class="flex flex-col bg-cover bg-center h-screen p-24 justify-evenly items-center lg:mb-20">
         <div>
             <h1 class="text-white lg:text-6xl">{{hero.title}}</h1>
             <h2 class="text-white mt-4 font-semibold  lg:text-3xl">{{hero.subtitle}}</h2>
         </div>
         <button class="border-arth-light-blue justify-center font-tinos mb-16">{{ $t("buttons.discover") }}</button>
-
     </div>
 
 </template>
@@ -21,10 +20,12 @@
             }
         );
             this.hero = {...response.data['data']};
+            this.image = {backgroundImage:`url(${this.hero.media_url})`}
         },
         data() {
             return {
-                hero: {}
+                hero: {},
+                image: {},
             }
         }
     }
