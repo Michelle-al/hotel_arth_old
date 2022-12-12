20c49ecdeda3122789bbda5265787d3239f486fa
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-12-12 15:41:51 +0100

    Add meta tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- TODO Meta descrition--}}
+    <meta name="description" content="Venez séjourner à l'Hôtel Arth. Situé près du lac d'Annecy, notre personnel
+    vous accueille pour vos séjour détente ou business. Dans une ambiance douce et reposante, profitez de nuits de
+    repos dans nos chambres parfaitement insonorisées. Le Chef vous fera découvrir ses délices. Envie d'une
+    expérience inoubliable ? Réservez votre accès à la piscine de l'hôtel, située sur le rooftop.">
+    <meta name="keywords" content="hôtel, repos, dépaysement, voyage, séjour, Annecy, lac, lac d'Annecy, dormir,
+    famille, déplacement professionnel, où dormir, hôtel-restaurant, suite, réception, piscine, rooftop, toit
+    accessible, vue sur les montagnes">
     <title>Hôtel Arth</title>
     <link rel="icon" type="image/png" href="{{ asset('storage/pictures/favicon.png') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
