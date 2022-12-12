5aae13efd0631371e724e0df087f3b37931c3beb
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-12-12 10:06:15 +0100

    BF - Reservation Option Database Seeding

--- database/seeders/ReservationsOptionSeeder.php
+++ database/seeders/ReservationsOptionSeeder.php
@@ -14,6 +14,6 @@ class ReservationsOptionSeeder extends Seeder
      */
     public function run()
     {
-        ReservationsOptionSeeder::factory(10)->create();
+        ReservationsOption::factory(10)->create();
     }
 }
