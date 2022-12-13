7c48852c99eec0ca4b9afe1f531153f9e4725ca6
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-12-13 14:16:31 +0100

    Finish front-end implementation with fake data

--- resources/js/Views/Reservation/Options.vue
+++ resources/js/Views/Reservation/Options.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="options__section">
         <!--    navigation breadcrumb-->
         <nav class="flex" aria-label="Breadcrumb">
             <ol class="inline-flex items-center space-x-1 md:space-x-3">
@@ -18,7 +18,8 @@
                             <path fill-rule="evenodd"
                                   d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                   clip-rule="evenodd"></path></svg>
-                        <router-link :to="{name : 'reservation'}" class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                        <router-link :to="{name : 'reservation'}"
+                                     class="ml-1 text-sm font-medium text-gray-500  hover:text-gray-900  dark:text-gray-400 dark:hover:text-white">
                             Réservation
                         </router-link>
                     </div>
@@ -32,75 +33,86 @@
                             <path fill-rule="evenodd"
                                   d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                   clip-rule="evenodd"></path></svg>
-                        <span class="ml-1 text-sm font-medium text-gray-700 md:ml-2 dark:text-gray-400">Options</span>
+                        <span
+                            class="ml-1 text-sm font-medium md:ml-2 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">Options</span>
                     </div>
                 </li>
             </ol>
         </nav>
         <h1 class="option__heading">{{$t(('options.title'))}}</h1>
 
-        <form v-on:submit.prevent="logInfos"
-              class="form-control flex flex-col align-center w-96 md:w-3/6 lg:w-4/12 mx-auto mt-12">
+        <div class="option__heading--recap"> <!--TODO - Fake data, implement data from DB-->
+            <p>{{ $t(('options.recapTitle'))}}</p>
+            <p>{{ $t(('options.recapDate'))}}</p>
+            <p>{{ $t(('options.recapRoom'))}}</p>
+        </div>
 
-            <div class="flex space mx-4">
+        <p class="option__heading--help">{{$t(('options.help'))}}</p>
+
+        <form v-on:submit.prevent="logInfos"
+              class="form flex flex-col align-center mt-4">
+
+            <div class="form-control flex flex-row flex-wrap mx-4 my-2">
                 <input type="checkbox"
                        id="optionPetitDejeuner"
                        checked="checked"
-                       class="checkbox"
-                       name="optionPetitDejeuner"
-                       v-model="form.optionPetitDejeuner">
-                <label for="optionPetitDejeuner" class="label ml-4">
+                       class="checkbox checkbox-xs"
+                       value="optionPetitDejeuner"
+                       v-model="formOptions">
+                <label for="optionPetitDejeuner" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionPetitDejeuner')}}</span>
                 </label>
             </div>
-            <div class="flex mx-4">
-                <input type="checkbox" id="optionMidi" class="checkbox" name="optionMidi" v-model="form.optionMidi">
-                <label for="optionMidi" class="label ml-4">
+            <div class="form-control flex flex-row mx-4 my-2">
+                <input type="checkbox" id="optionMidi" class="checkbox checkbox-sm" value="optionMidi"
+                       v-model="formOptions">
+                <label for="optionMidi" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionMidi')}}</span>
                 </label>
             </div>
 
-            <div class="flex mx-4">
-                <input type="checkbox" id="optionSoir" class="checkbox" name="optionSoir" v-model="form.optionSoir">
-                <label for="optionSoir" class="label ml-4">
+            <div class="form-control flex flex-row mx-4 my-2">
+                <input type="checkbox" id="optionSoir" class="checkbox checkbox-sm" value="optionSoir"
+                       v-model="formOptions">
+                <label for="optionSoir" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionSoir')}}</span>
                 </label>
             </div>
-            <div class="flex mx-4">
+            <div class="form-control flex flex-row mx-4 my-2">
 
-                <input type="checkbox" id="optionMidiEtSoir" class="checkbox" name="optionMidiEtSoir"
-                       v-model="form.optionSoir">
-                <label for="optionMidiEtSoir" class="label ml-4">
+                <input type="checkbox" id="optionMidiEtSoir" class="checkbox checkbox-sm" value="optionMidiEtSoir"
+                       v-model="formOptions">
+                <label for="optionMidiEtSoir" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionMidiEtSoir')}}</span>
                 </label>
             </div>
 
-            <div class="flex mx-4">
-                <input type="checkbox" id="optionPressing" class="checkbox" name="optionPressing"
-                       v-model="form.optionPressing">
-                <label for="optionPressing" class="label ml-4">
+            <div class="form-control flex flex-row mx-4 my-2">
+                <input type="checkbox" id="optionPressing" class="checkbox checkbox-sm" value="optionPressing"
+                       v-model="formOptions">
+                <label for="optionPressing" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionPressing')}}</span>
                 </label>
 
             </div>
-            <div class="flex mx-4">
-                <input type="checkbox" id="optionCanalPlus" class="checkbox" name="optionCanalPlus"
-                       v-model="form.optionCanalPlus">
-                <label for="optionCanalPlus" class="label ml-4">
+            <div class="form-control flex flex-row mx-4 my-2">
+                <input type="checkbox" id="optionCanalPlus" class="checkbox checkbox-sm" value="optionCanalPlus"
+                       v-model="formOptions">
+                <label for="optionCanalPlus" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionCanalPlus')}}</span>
                 </label>
 
             </div>
-            <div class="flex mx-4">
-                <input type="checkbox" id="optionSwimmingPool" class="checkbox" name="optionSwimmingPool"
-                       v-model="form.optionSwimmingPool">
-                <label for="optionSwimmingPool" class="label ml-4">
+            <div class="form-control flex flex-row mx-4 my-2">
+                <input type="checkbox" id="optionSwimmingPool" class="checkbox checkbox-sm" value="optionSwimmingPool"
+                       v-model="formOptions">
+                <label for="optionSwimmingPool" class="label cursor-pointer ml-4">
                     <span class="label-text">{{ $t('options.optionSwimmingPool')}}</span>
                 </label>
             </div>
 
             <p class="mt-6 text-center">{{ $t('options.totalAmount') }}
-                <span class="text-red-700">EXEMPLE 580</span>
+                <span class="font-bold">580</span> <!-- TODO - Fake data, implement data from DB -->
                 <span> €</span>
             </p>
 
@@ -115,23 +127,19 @@
 
 <script>
 import reservation from "./Reservation.vue";
+import options from "./Options.vue";
 
 export default {
     name: "Options.vue",
     data() {
         return {
-            form: {
-                optionPetitDejeuner:'',
-                optionMidi:'',
-                optionSoir:'',
-                optionMidiEtSoir:'',
-                optionPressing:'',
-                optionCanalPlus:'',
-                optionSwimmingPool:'',
-            }
+            formOptions: []
         }
     },
     methods: {
+        options() {
+            return options
+        },
         reservation() {
             return reservation
         },
@@ -143,11 +151,23 @@ export default {
 </script>
 
 <style scoped>
+    .options__section {
+        @apply w-11/12 sm:w-96 md:w-3/6 lg:w-4/12 mx-auto
+    }
+
     .option__heading {
         @apply mt-6
     }
 
-    .form-control {
-        @apply border-2 border-red-700
+    .option__heading--recap {
+        @apply bg-arth-light-blue mx-auto mt-12 py-3 text-center
     }
+
+    .option__heading--help {
+        @apply mt-12 text-center font-montserrat
+    }
+
+    /*.form {
+        @apply
+    }*/
 </style>
