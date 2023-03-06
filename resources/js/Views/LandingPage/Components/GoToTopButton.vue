2dd4338f4a375f8ec672cd6fa21be8cdaa90f6dc
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2023-03-06 15:12:01 +0100

    Add go to top button

---
+++ resources/js/Views/LandingPage/Components/GoToTopButton.vue
@@ -0,0 +1,46 @@
+<template>
+    <router-link to="#header">
+        <button type="button" class="">
+            <img :src="image.source" :alt="image.alt">
+        </button>
+    </router-link>
+
+</template>
+
+<script>
+export default {
+    name: "GoToTopButton",
+    props: {
+    },
+    data() {
+        return {
+            image: {
+                source: "storage/advantages/arrow-down.png",
+                alt: "Retourner en haut de la page",
+            }
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    button {
+        position: fixed;
+        bottom: 0.8rem;
+        right: 0.8rem;
+        z-index: 99;
+        width: 70px;
+        height: auto;
+        text-align: center;
+
+        @apply bg-arth-green hover:bg-arth-light-blue border-arth-green focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center;
+    }
+
+    img {
+        transform: rotate(180deg);
+        width: 40px;
+        height: auto;
+    }
+
+</style>
