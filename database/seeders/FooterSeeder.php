9eb9f9d91fe5fe25217568b317e7234442133fd9
Michelle <michelle.alabi@gmail.com>
2023-03-18 22:23:29 +0100

    update of factory and seeder

--- database/seeders/FooterSeeder.php
+++ database/seeders/FooterSeeder.php
@@ -16,19 +16,19 @@ class FooterSeeder extends Seeder
     {
         DB::table('footers')->insert([
             [
-                "column_number" => 1,
-                "entry_name" => json_encode(["fr" => "Nous contacter", "en" => "Contact us"]),
-                "url_redirection" => "/contact",
+                'column_number' => 1,
+                'entry_name' => json_encode(['fr' => 'Nous contacter', 'en' => 'Contact us']),
+                'url_redirection' => '/contact',
             ],
             [
-                "column_number" => 1,
-                "entry_name" => json_encode(["fr" => "A propos", "en" => "About"]),
-                "url_redirection" => "/about",
+                'column_number' => 1,
+                'entry_name' => json_encode(['fr' => 'A propos', 'en' => 'About']),
+                'url_redirection' => '/about',
             ],
             [
-                "column_number" => 2,
-                "entry_name" => json_encode(["fr" => "Promotions", "en" => "Sales"]),
-                "url_redirection" => "/sales",
+                'column_number' => 2,
+                'entry_name' => json_encode(['fr' => 'Promotions', 'en' => 'Sales']),
+                'url_redirection' => '/sales',
             ],
 
         ]);
